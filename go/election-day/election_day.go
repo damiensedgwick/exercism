f62d65c4cdb2c44c4905a290c5b443dc36bb63b3
Damien Sedgwick <damienksedgwick@gmail.com>
2024-03-21 18:41:01 +0000

    implemented DecrementVotesOfCandidate

--- go/election-day/election_day.go
+++ go/election-day/election_day.go
@@ -39,5 +39,8 @@ func DisplayResult(result *ElectionResult) string {
 
 // DecrementVotesOfCandidate decrements by one the vote count of a candidate in a map.
 func DecrementVotesOfCandidate(results map[string]int, candidate string) {
-	panic("Please implement the DecrementVotesOfCandidate() function")
+	_, exists := results[candidate]
+	if exists {
+		results[candidate] -= 1
+	}
 }
