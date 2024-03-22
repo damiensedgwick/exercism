89abd384d8c47f1d43d98a376b329938108e7cad
Damien Sedgwick <damienksedgwick@gmail.com>
2024-03-22 08:02:20 +0000

    wip: elon's toys

--- go/elons-toys/elons_toys.go
+++ go/elons-toys/elons_toys.go
@@ -1,9 +1,12 @@
 package elon
 
-func (c Car) Drive() {}
+func (c *Car) Drive() {
+	c.battery -= c.batteryDrain
+	c.distance += c.speed
+}
 
-func (c Car) DisplayDistance() string {}
+func (c *Car) DisplayDistance() string {}
 
-func (c Car) DisplayBattery() string {}
+func (c *Car) DisplayBattery() string {}
 
-func (c Car) CanFinish(distance int) bool {}
+func (c *Car) CanFinish(distance int) bool {}
